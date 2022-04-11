607e8af8330ac3106fd9e1cd0937918e4a439a22
Crush <1471062948@qq.com>
2022-04-11 22:41:40 +0800

    feat: 配置一级路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './app.less';
-
+import Layout from './pages/Layout';
+import Login from './pages/Login';
 function App() {
   return (
-    <div className="App">
-      app
-    </div>
+    // 路由配置
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          {/* 创建路由path和组件对应关系 */}
+          <Route path='/login' element={<Login />} />
+          <Route path='/' element={<Layout />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+    // <div className="App">
+    //   app
+    // </div>
   );
 }
 
