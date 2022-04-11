66dd5bb674be5cc71b084d630e2dcf1ba3d3bf60
Crush <1471062948@qq.com>
2022-04-11 23:34:23 +0800

    feat: 配置src路径别名为@

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import 'antd/dist/antd.min.css'
 import './app.less';
 import Layout from './pages/Layout';
-import Login from './pages/Login';
+import Login from '@/pages/Login';
+// import 
 import { Button } from 'antd'
 function App() {
   return (
     // 路由配置
     <BrowserRouter>
       <div className="App">
-        <Button type='primary'>开玩笑</Button>
+        <Button type='primary'>这是个button</Button>
         <Routes>
           <Route path='/login' element={<Login />} />
           <Route path='/' element={<Layout />} />
